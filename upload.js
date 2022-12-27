dd3a6456da0ea02821c4ad0a6ed80f41ceb9bc0c
Atiqul Hoque <atiqbaqi@gmail.com>
2022-12-27 16:57:56 +0600

    file type, file size, file name formatting

--- upload.js
+++ upload.js
@@ -1,9 +1,24 @@
 const AWS = require('aws-sdk');
 const express = require('express');
 const app = express();
+const path = require("path");
 //const fs = require('fs');
 const multer = require('multer'); 
-const upload = multer();
+
+const MIME_TYPES = ["image/png", "image/jpg", "image/jpeg"]; // add "application/pdf" for pdf files
+const MAX_SIZE = 1024 * 1024 * 5;//6MB
+const upload = multer({
+  limits: {fileSize: MAX_SIZE},
+  fileFilter: (req, file, cb) => {
+    if (MIME_TYPES.includes(file.mimetype)) {
+        cb(null, true);
+    } else {
+        // req.body[file.fieldname] = file.originalname
+        req.upload_error = true;
+        cb(new Error("Only .png, .jpg, or .jpeg format allowed!"));
+    }
+}, 
+});
 require('dotenv').config();
 
 const app_port = process.env.APP_PORT || 8080;
@@ -11,6 +26,9 @@ const app_port = process.env.APP_PORT || 8080;
 app.post('/upload', upload.single('image'), (req, res) => {
   // Read the image file from the request
   const file = req.file;
+
+  const fileExt = path.extname(file.originalname);
+        const fileName = Date.now()+fileExt;
   //fs.writeFileSync(`tmp/${file.originalname}`, file.buffer); // save the image in tmp folder
   // Configure the AWS S3 client
   const s3 = new AWS.S3({
@@ -22,7 +40,7 @@ app.post('/upload', upload.single('image'), (req, res) => {
   // Set the parameters for the S3 upload
   const params = {
     Bucket: process.env.AWS_S3_BUCKET,
-    Key: `uploads/${file.originalname}`,
+    Key: `uploads/${fileName}`,
     Body: file.buffer,
     //ACL (Access Control List) is a predefined policy in Amazon S3 (Simple Storage Service) that allows any user to read an object in a bucket.
     ACL: 'public-read',
@@ -35,6 +53,7 @@ app.post('/upload', upload.single('image'), (req, res) => {
       res.status(500).send(error);
     } else {
       // Return the URL of the uploaded image
+      console.log(data.Location);
       res.send(data.Location);
     }
   });
